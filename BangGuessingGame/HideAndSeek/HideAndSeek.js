8683fba25ba95665c8d48abcb38f8e009dd31964
ThogerBang <thoger.bang.petersen@gmail.com>
2024-08-23 21:52:50 +0200

    hide and seek part 1

--- BangGuessingGame/HideAndSeek/HideAndSeek.js
+++ BangGuessingGame/HideAndSeek/HideAndSeek.js
@@ -4,27 +4,40 @@ const oAuth = "bbxuasj3p1vaid6o0h2oye3lvnwh3n";
 const nick = "bang";
 
 const CounterDown = document.getElementById('CounterDown');
-const WordTable = document.getElementById("WordTable");
 const FakeText = document.getElementById('FakeText');
 const FakeUser = document.getElementById('FakeUser');
+const MiddleBox = document.getElementById('MiddleBox');
+const SmallBoxes = [document.getElementById('SmallBox1'),document.getElementById('SmallBox2'),document.getElementById('SmallBox3'),document.getElementById('SmallBox4'),document.getElementById('SmallBox6'),document.getElementById('SmallBox7'),document.getElementById('SmallBox8'),document.getElementById('SmallBox9')];
 
-const gameDuration = 90;
+const isInteger = /^-?\d+$/;
+
+const hidingTime = 20;
 
 var players =  JSON.parse(localStorage.getItem("players"));
 var isPlaying = true;
+var inHidingphase = true;
 var counter = 0;
-var countdown = gameDuration;
+var countdown = hidingTime;
 var isCounting = true;
 var words = [];
 var thisPlayers = [];
+var playerBoxes = [];
 for (let i = 0; i<players.length; i++){
   if(players[i].alive){
-      thisPlayers.push({player:players[i],spot:0});
+    thisPlayers.push({player:players[i],spot:0});
+    playerBoxes.push(document.createElement('div')); 
+    playerBoxes[i].classList.add('small-element');
+    const posX = Math.floor(Math.random() * (MiddleBox.clientWidth - 20));
+    const posY = Math.floor(Math.random() * (MiddleBox.clientHeight - 20));
+    playerBoxes[i].style.left = `${posX}px`;
+    playerBoxes[i].style.top = `${posY}px`;
+    playerBoxes[i].style.backgroundColor = thisPlayers[i].player.color;
+    playerBoxes[i].textContent = thisPlayers[i].player.number;
+    MiddleBox.appendChild(playerBoxes[i]);
   }
 }
-
 const myInterval = setInterval(timeAction, 100);
-CounterDown.innerHTML = ""+gameDuration;
+CounterDown.innerHTML = ""+hidingTime;
 
 socket.addEventListener('open', (event) => {
     socket.send(`PASS oauth:${oAuth}`);
@@ -49,10 +62,23 @@ FakeText.addEventListener('keydown', (event) => {
       handleMessage(FakeUser.value,FakeText.value)
   }
 });
+FakeUser.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+      handleMessage(FakeUser.value,FakeText.value)
+  }
+});
 
 function handleMessage(user, message){
-  if(isAlivePlayer(user) && isPlaying && message.split(" ").length <=1){
+  if(isAlivePlayer(user) && isPlaying && inHidingphase && message.split(" ").length <=1){
     let play = thisPlayers.find(p => p.player.name === user);
+    if (isInteger.test(message)){
+      let integer = parseInt(message);
+      if (integer>0 && integer <9){
+        play.spot = integer;
+        playerBoxes[thisPlayers.indexOf(play)].remove();
+        playerBoxes[thisPlayers.indexOf(play)] = "null";
+      } 
+    }
   }  
 }
 
