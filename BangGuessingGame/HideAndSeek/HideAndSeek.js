edebc0a802316b8d8353c9a47452c94d157601aa
ThogerBang <thoger.bang.petersen@gmail.com>
2024-09-05 13:58:07 +0200

    HAS finished

--- BangGuessingGame/HideAndSeek/HideAndSeek.js
+++ BangGuessingGame/HideAndSeek/HideAndSeek.js
@@ -13,6 +13,7 @@ const isInteger = /^-?\d+$/;
 
 const hidingTime = 15;
 const killTime = 2;
+const defaultSpot = 10;
 
 var players =  JSON.parse(localStorage.getItem("players"));
 var isPlaying = true;
@@ -23,13 +24,13 @@ var countdown = hidingTime;
 var isCounting = true;
 var words = [];
 var thisPlayers = [];
-var eliminated = [];
+var found = [];
 var playerBoxes = [];
 var revealed = [];
 
 for (let i = 0; i<players.length; i++){
   if(players[i].alive){
-    thisPlayers.push({player:players[i],spot:10});
+    thisPlayers.push({player:players[i],spot:defaultSpot});
     playerBoxes.push(document.createElement('div')); 
     playerBoxes[i].classList.add('small-element');
     const posX = Math.floor(Math.random() * (MiddleBox.clientWidth - 20));
@@ -39,6 +40,7 @@ for (let i = 0; i<players.length; i++){
     playerBoxes[i].style.backgroundColor = thisPlayers[i].player.color;
     playerBoxes[i].textContent = thisPlayers[i].player.number;
     MiddleBox.appendChild(playerBoxes[i]);
+    found.push(false);
   }
 }
 const myInterval = setInterval(timeAction, 100);
@@ -83,22 +85,21 @@ for (let i = 0; i < SmallBoxes.length; i++){
 function handleMessage(user, message){
   if(isAlivePlayer(user) && isPlaying && inHidingphase && message.split(" ").length <=1){
     let play = thisPlayers.find(p => p.player.name === user);
-    if (isInteger.test(message)){
+    if (isInteger.test(message) && !found[play.player.number]){
       let integer = (parseInt(message));
       if (integer>0 && integer <9 && !revealed.includes(integer-1)){
         play.spot = integer-1;
-        //playerBoxes[thisPlayers.indexOf(play)].style.display = "none";
-        window.requestAnimationFrame(() => {
+        playerBoxes[thisPlayers.indexOf(play)].style.display = "none";
+        /*window.requestAnimationFrame(() => {
           playerBoxes[thisPlayers.indexOf(play)].left = "300px";
           playerBoxes[thisPlayers.indexOf(play)].style.top = "200px";
-      });
+      });*/
       } 
     }
   }  
 }
 
 function boxClicked(i){
-  console.log(revealed);
   if(!inHidingphase && !isKilling &&!revealed.includes(i)){
     SmallBoxes[i].children[0].style.display = "none";
     revealed.push(i);
@@ -119,6 +120,11 @@ function revealBox(j){
       playerBoxes[i].style.left = `${posX}px`;
       playerBoxes[i].style.top = `${posY}px`;
       SmallBoxes[j].appendChild(playerBoxes[i]);
+      found[i] = true;
+    }
+    if(thisPlayers[i].spot === defaultSpot){
+      playerBoxes[i].style.display = "none";
+      found[i] = true;
     }
   }
 }
@@ -126,6 +132,12 @@ function revealBox(j){
 
 function endGame(){
   isPlaying = false;
+  let eliminated = [];
+  for(let i = 0; i < thisPlayers.length; i++){
+    if(found[i]){
+      eliminated.push(thisPlayers[i].player);
+    }
+  }
   localStorage.setItem("eliminated",JSON.stringify(eliminated));
   window.location.href = "../GameSelector.html";
 }
@@ -133,6 +145,48 @@ function endGame(){
 function isAlivePlayer(user){
   return thisPlayers.some(p => p.player.name === user);
 }
+
+function countDownAction(){
+  if(inHidingphase){
+    endHidingPhase();
+  }
+  else if(isKilling){
+    endKillingPhase();
+  }
+}
+
+function endKillingPhase(){
+  if (revealed.length >= 3){
+    endGame();
+  }else{
+    inHidingphase = true;
+    countdown = hidingTime;
+  }
+  for (let i = 0; i<thisPlayers.length; i++){
+    if(!found[i]){
+      playerBoxes[i].style.display = "block";
+      //const posY = rect.top - (SmallBoxes[j].clientHeight/2) + 10;
+      const posX = Math.floor(Math.random() * (MiddleBox.clientWidth - 20));
+      const posY = Math.floor(Math.random() * (MiddleBox.clientHeight - 20));
+      playerBoxes[i].style.left = `${posX}px`;
+      playerBoxes[i].style.top = `${posY}px`;
+      MiddleBox.appendChild(playerBoxes[i]);
+      thisPlayers[i].spot = defaultSpot;
+    }
+  }
+}
+
+function endHidingPhase(){
+  inHidingphase = false;
+  isKilling = false;
+  for (let i = 0; i<thisPlayers.length; i++){
+    if(thisPlayers[i].spot === defaultSpot){
+      playerBoxes[i].style.display = "none";
+      found[i] = true;
+    }
+  }
+}
+
 
 function timeAction(){
   if (isPlaying){
@@ -144,18 +198,7 @@ function timeAction(){
             CounterDown.innerHTML = countdown;
           }
           if(countdown <= 0){
-            if(inHidingphase){
-              inHidingphase = false;
-              isKilling = false;
-            }
-            else if(isKilling){
-              if (revealed.length >= 3){
-                endGame();
-              }else{
-                inHidingphase = true;
-                countdown = hidingTime;
-              }
-            }
+            countDownAction();
           }
           counter = 0;
       }
